83378bac08bf072ed5de0655f8ccdc224d489ac3
Huaji <15666613559@163.com>
2020-02-22 14:19:27 +0800

    finish generate ast

--- code/src/include/ast_ap.h
+++ code/src/include/ast_ap.h
@@ -7,9 +7,12 @@
 #include "sql_expr.h"
 #include <memory>
 #include <vector>
+#include <map>
 
 using std::vector;
 using std::shared_ptr;
+using std::map;
+using std::pair;
 
 /*
  * this file includes
@@ -22,20 +25,30 @@ namespace DB::ast{
     //op nodes of ap
     enum class ap_op_t_t { EMIT, PROJECT, FILTER, JOIN, TABLE };
 
+    using APMap = map<pair<string, string>, int>; // need reset
+
     struct APBaseOp {
-        APBaseOp(ap_op_t_t op_t) : op_t_(op_t) {}
+        APBaseOp(ap_op_t_t op_t, APBaseOp *parentOp = nullptr)
+            : op_t_(op_t), _parentOp(parentOp) {}
         virtual ~APBaseOp() = 0;
 
-        //virtual void produce() {};
-        //virtual void consume(attributes,source) {};
+        virtual void produce() {};
+        // source are used to check left/right child, map is constructed from bottom to top
+        virtual void consume(APBaseOp *source, APMap &map) {};
+
+        void setParentOp(APBaseOp *parentOp) { _parentOp = parentOp; };
 
         ap_op_t_t op_t_;
+        APBaseOp* _parentOp;
     };
 
     struct APEmitOp : public  APBaseOp {
-        APEmitOp() : APBaseOp(ap_op_t_t::EMIT) {}
+        APEmitOp(APBaseOp *op, int hashTableCount)
+            : APBaseOp(ap_op_t_t::EMIT), _op(op), _hashTableCount(hashTableCount) {}
         virtual ~APEmitOp() {}
 
+        int _hashTableCount;
+        APBaseOp *_op;
     };
 
     struct APProjectOp : public APBaseOp {
@@ -45,22 +58,28 @@ namespace DB::ast{
     };
 
     struct APFilterOp : public APBaseOp {
-        APFilterOp(ast::BaseExpr* whereExpr) : APBaseOp(ap_op_t_t::FILTER) {}
+        APFilterOp(APBaseOp *tableOp, shared_ptr<BaseExpr> condition)
+            : APBaseOp(ap_op_t_t::FILTER), _condition(condition) {}
         virtual ~APFilterOp() { }
 
+        shared_ptr<BaseExpr> _condition;
     };
 
     struct APJoinOp : public APBaseOp {
-        APJoinOp() : APBaseOp(ap_op_t_t::JOIN) {}
+        APJoinOp(APBaseOp *table1, APBaseOp *table2, shared_ptr<BaseExpr> condition, int hashTableIndex)
+            : APBaseOp(ap_op_t_t::JOIN), _table1(table1), _table2(table2),
+            _condition(condition), _hashTableIndex(hashTableIndex) {}
         virtual ~APJoinOp() {}
 
+        int _hashTableIndex;
+        APBaseOp *_table1;
+        APBaseOp *_table2;
+        shared_ptr<BaseExpr> _condition;
     };
 
     struct APTableOp : public APBaseOp {
-        APTableOp(const string tableName) : APBaseOp(ap_op_t_t::TABLE), _tableName(tableName) {}
+        APTableOp(const string tableName);
         virtual ~APTableOp() {}
-
-        string _tableName;
     };
 
 
@@ -73,6 +92,8 @@ namespace DB::ast{
      * used before actually generation
      * throw exception if check fails
      */
+    void apCheckSingle(shared_ptr<BaseExpr> &condition);
+
     void apCheckVisit(vector<shared_ptr<BaseExpr>> &conditions);
 
 
@@ -84,7 +105,7 @@ namespace DB::ast{
      *  default select all
      *  currently don't handle select
      */
-    void generateAst(vector<string> tables, vector<shared_ptr<BaseExpr>> &conditions, vector<Column> columns = {});
+    shared_ptr<APEmitOp> generateAst(const vector<string> &tables, const vector<shared_ptr<BaseExpr>> &conditions, vector<Column> columns = {});
 
 
 }
